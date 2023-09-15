9ee7e265cf6540a05040e5e643e17f5553ff8cad
vajin1125 <31552910+vajin1125@users.noreply.github.com>
2023-09-15 17:26:04 +0300

    added validation of dto and create update-user dto

--- src/DTO/user/create-user.dto.ts
+++ src/DTO/user/create-user.dto.ts
@@ -1,6 +1,14 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
 export class CreateUserDto {
+  @IsNotEmpty()
   first_name: string;
+
+  @IsNotEmpty()
   last_name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   password: string;
 }
--- src/DTO/user/login-user.dto.ts
+++ src/DTO/user/login-user.dto.ts
@@ -1,4 +1,9 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class LoginUserDto {
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   password: string;
 }
